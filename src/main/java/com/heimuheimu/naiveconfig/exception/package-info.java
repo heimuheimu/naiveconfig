0ab44bf4828c7fd6f935cf0cea7a5b008033bef7
heimuheimu <boyszz2009@163.com>
2018-01-18 23:19:50 +0800

    Complete javadoc for `exception` package.

--- src/main/java/com/heimuheimu/naiveconfig/exception/package-info.java
+++ src/main/java/com/heimuheimu/naiveconfig/exception/package-info.java
@@ -22,23 +22,9 @@
  * SOFTWARE.
  */
 
-package com.heimuheimu.naiveconfig.exception;
-
 /**
- * 从配置中心获取配置或者设置配置时遇到错误，将会抛出此异常。
+ * 提供 NaiveConfig 异常类定义。
  *
  * @author heimuheimu
  */
-public class NaiveConfigException extends RuntimeException {
-
-    private static final long serialVersionUID = 6305753996453818689L;
-
-    public NaiveConfigException(String message) {
-        super(message);
-    }
-
-    public NaiveConfigException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
+package com.heimuheimu.naiveconfig.exception;

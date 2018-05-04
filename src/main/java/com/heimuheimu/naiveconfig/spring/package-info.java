d4b4129f731ac4a79df3fd4a67fe4e5664cf49ba
heimuheimu <boyszz2009@163.com>
2018-05-04 13:23:32 +0800

    add package-info.java.

---
+++ src/main/java/com/heimuheimu/naiveconfig/spring/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 heimuheimu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * 使用 {@code PropertyRedisConfigurer} 可通过 Redis 远程加载 Spring 启动所需的配置信息，例如 DB 地址、缓存地址等，
+ * {@code PropertyRedisConfigurer} 将会对 "({" 开头，并以 "})" 结尾的变量进行匹配替换。
+ *
+ * @author heimuheimu
+ */
+package com.heimuheimu.naiveconfig.spring;
